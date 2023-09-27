bbdd5c721bc5405d711ae3ce822949b540418ac9
Edoardo <edoardo.varesco@buonarroti.tn.it>
2023-09-27 17:07:06 +0200

    feat: Dichiarate le variabili altezza e raggio

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Tubo tubo;
+        double altezza;
+        double raggio;
+
 
 
     }
