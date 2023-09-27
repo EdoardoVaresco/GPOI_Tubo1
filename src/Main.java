83ddf1e9501a4e7604c15ec532f64854b229cc7b
Edoardo <edoardo.varesco@buonarroti.tn.it>
2023-09-27 17:17:34 +0200

    feat: Test dei metodi calcSuperficie e calcVolume

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,10 @@ public class Main {
         raggio = input.nextDouble();
         System.out.println("Inserisci l'altezza del tubo");
         altezza = input.nextDouble();
+
+        tubo = new Tubo(altezza, raggio);
+        System.out.println("La superficie del tubo è: " + tubo.calcSuperficie());
+        System.out.println("Il volume del tubo è: " + tubo.calcVolume());
+
     }
 }
