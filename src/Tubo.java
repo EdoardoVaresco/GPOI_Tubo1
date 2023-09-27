c6e4d70ff626cbab234567d08647d3e0c7f9d3d0
Edoardo <edoardo.varesco@buonarroti.tn.it>
2023-09-27 16:41:50 +0200

    feat: Aggiunto metodo costruttore Tubo

--- src/Tubo.java
+++ src/Tubo.java
@@ -1,4 +1,11 @@
 public class Tubo {
     private double altezza;
     private double raggio;
+
+    public Tubo(double altezza, double raggio){
+        this.altezza = altezza;
+        this.raggio = raggio;
+    }
+
+
 }
